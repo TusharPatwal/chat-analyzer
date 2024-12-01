f8bd770860f3638000af791242eefaa1ce9fa8fb
TusharPatwal <patwaltushar001@gmail.com>
2024-12-01 23:31:22 +0530

    analysis on monthly & daily data

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import preprocessor
 import helper
+import matplotlib.pyplot as plt
 
 st.sidebar.title('Whatsapp Chat Analyzer')
 
@@ -9,7 +10,6 @@ if upload_file is not None:
     bytes_data = upload_file.getvalue()
     data = bytes_data.decode('utf-8')
     df = preprocessor.preprocess(data)
-    st.dataframe(df)
 
     # fetch unique users
     user_list = df['users'].unique().tolist()
@@ -24,11 +24,11 @@ if upload_file is not None:
         # Stats Area
         num_messages, words, num_media_msg, link_shared = helper.fetch_stats(
             selected_user, df)
-
+        st.title('Top Statistics')
         col1, col2, col3, col4 = st.columns(4)
 
         with col1:
-            st.header('Total Messages')
+            st.header('Total Msgs')
             st.title(num_messages)
 
         with col2:
@@ -42,6 +42,17 @@ if upload_file is not None:
         with col4:
             st.header('Links Shared')
             st.title(link_shared)
+            
+    # monthly timeline
+    st.title('Monthly Timeline')
+    timeline = helper.monthly_timeline(selected_user, df)
+    st.plotly_chart(timeline)
+    
+    # daily timeline
+    st.title('Daily Timeline')
+    d_timeline = helper.daily_timeline(selected_user, df)
+    st.plotly_chart(d_timeline)
+    
 
     # finding the busiest users in the group(group level)
     if selected_user == 'Overall':
@@ -65,3 +76,19 @@ if upload_file is not None:
     most_common_words = helper.most_common_words(selected_user, df)
 
     st.plotly_chart(most_common_words)
+    
+    # emoji analysis
+    emoji_df, fig = helper.emoji_helper(selected_user, df)
+    st.title('Emoji Analysis')
+    
+    col1, col2 = st.columns(2)
+    
+    with col1:
+        st.dataframe(emoji_df)
+
+    with col2:
+        st.plotly_chart(fig)
+        
+    # monthly timeline
+    st.title("Monthly Timeline")
+    
